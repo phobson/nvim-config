005496422511538e1759b205dfd89191f7c878aa
Paul Hobson <phobson@herrerainc.com>
2026-02-09 09:40:27 -0800

    add file tree toggle keymaps

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -9,10 +9,11 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format file using the LSP" })
-vim.keymap.set("n", "<leader>wwq", ":wqall<cr>")
-vim.keymap.set("n", "<leader>www", ":wall<cr>")
-vim.keymap.set("n", "<leader>xxx", ":qa!<cr>")
-
+vim.keymap.set("n", "<leader>wwq", ":wqall<cr>", {desc = "Save everything and quit"})
+vim.keymap.set("n", "<leader>www", ":wall<cr>", {desc = "Save everthing"})
+vim.keymap.set("n", "<leader>xxx", ":qa!<cr>", {desc = "Bail out! (quit and don't save"})
+vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle", {desc = "[T]oggle [F]ile Tree"})
+vim.keymap.set("n", "<leader>tg", ":NvimTreeFindFile", {desc = "File [T]reem [G]o to file"})
 -- vim.keymap.set("n", "<leader>se", function()
 -- 	local new_config = not vim.diagnostic.config().virtual_lines
 -- 	vim.diagnostic.config({ virtual_lines = new_config })
