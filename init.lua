7ad412c745ea9212d9db3249f173dd73e7197415
Paul Hobson <phobson@herrerainc.com>
2026-02-17 12:16:35 -0800

    apply colorscheme

--- init.lua
+++ init.lua
@@ -2,16 +2,18 @@ require("config.options")
 require("config.keybinds")
 require("config.lazy")
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
--- vim.opt.winborder = "rounded"
-vim.cmd.colorscheme "everforest"
--- vim.cmd.colorscheme("onedark")
--- vim.cmd.colorscheme "everforest"
--- vim.cmd.colorscheme "catppuccin"
+if package.config.sub(1, 1) == "/" then
+    -- not windows
+	vim.cmd.colorscheme("everforest")
+else
+    -- windows
+	vim.cmd.colorscheme("onedark")
+end
