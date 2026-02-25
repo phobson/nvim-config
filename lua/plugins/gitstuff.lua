9aedd6a6ba206e8744e55ec74f68a6fcf6f14d74
Paul Hobson <phobson@herrerainc.com>
2026-02-24 22:51:59 -0800

    actually get the git stuff

---
+++ lua/plugins/gitstuff.lua
@@ -0,0 +1,16 @@
+return {
+    { "tpope/vim-fugitive" }, -- git
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>gdi", ":Gitsigns preview_hunk_inline<CR>", { desc = "[g]it [d]iff [i]nline" })
+            vim.keymap.set(
+                "n",
+                "<leader>gbi",
+                ":Gitsigns toggle_current_line_blame<CR>",
+                { desc = "[g]it [b]lame [i]nline" }
+            )
+        end,
+    },
+}
